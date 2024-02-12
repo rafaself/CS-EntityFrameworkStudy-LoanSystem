3238cc9765e784f206f6b1f8c6902244f288293a
rafael <rafaondjango@gmail.com>
2024-02-12 08:05:36 -0300

    feat: começando a implementar o primeiro CRUD

---
+++ LoanSystem/Repositories/CustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using LoanSystem.Models;
+using LoanSystem.Repositories.Interfaces;
+
+namespace LoanSystem.Repositories;
+
+internal class CustomerRepository : ICostumerRepository
+{
+
+    private CustomDbContext _dbContext;
+
+    public CustomerRepository(CustomDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    public void Create(Customer customer)
+    {
+        _dbContext.Add(customer);   
+    }
+}
---
+++ LoanSystem/Repositories/Interfaces/ICostumerRepository.cs
@@ -0,0 +1,8 @@
+﻿using LoanSystem.Models;
+
+namespace LoanSystem.Repositories.Interfaces;
+
+public interface ICostumerRepository  
+{
+    void Create(Customer customer);
+}
