cb2b02b82adadc87b7a1f70798973ca2d75afc63
rafael <rafaondjango@gmail.com>
2024-02-13 09:19:08 -0300

    feat: migrações devido a pequena mudança de ID para Id

--- LoanSystem/Migrations/20240213121840_SmallChangesOnIDVars.Designer.cs
+++ LoanSystem/Migrations/20240213121840_SmallChangesOnIDVars.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LoanSystem;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LoanSystem.Migrations
 {
     [DbContext(typeof(CustomDbContext))]
-    partial class CustomDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240213121840_SmallChangesOnIDVars")]
+    partial class SmallChangesOnIDVars
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LoanSystem/Migrations/20240213121840_SmallChangesOnIDVars.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoanSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class SmallChangesOnIDVars : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Loans",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Customers",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Addresses",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Loans",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Customers",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Addresses",
+                newName: "ID");
+        }
+    }
+}
